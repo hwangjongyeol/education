b7046a78f7a502aad300b574035696625a128c1b
hwangjong-yeol <kuitrin@gmail.com>
2025-06-27 17:47:55 +0900

    수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,20 @@
 // src/pages/Home.jsx
-import styles from './Home.module.scss';
-import MainLayout from '../layouts/MainLayout';
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from './Home.module.scss';   // 스타일 import
 
 export default function Home() {
     return (
-        <MainLayout>
-            <div className={styles.container}>
-                <h1>📚 학습</h1>
-                <div className={styles.linkWrapper}>
-                    <div className={styles.linkCard}>
-                        <Link to="/english">정원이 영어</Link>
-                    </div>
-                    <div className={styles.linkCard}>
-                        <Link to="/math">지원이 수학</Link>
-                    </div>
+        <div className={styles.container}>
+            <h1>📚 학습</h1>
+            <div className={styles.linkWrapper}>
+                <div className={styles.linkCard}>
+                    <Link to="/english">정원이 영어</Link>
+                </div>
+                <div className={styles.linkCard}>
+                    <Link to="/math">지원이 수학</Link>
                 </div>
             </div>
-        </MainLayout>
+        </div>
     );
 }
